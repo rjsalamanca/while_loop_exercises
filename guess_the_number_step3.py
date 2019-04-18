8173c80c8eb04b6b0d8ae577ea3efbcccd3f4c50
RJ Salamanca <salamancaronito@gmail.com>
2019-04-17 22:41:12 -0400

    added step 3

---
+++ guess_the_number_step3.py
@@ -0,0 +1,15 @@
+import random
+
+secret_number = random.randint(1,10)
+guessed_correctly = False
+print('I am thinking of a number between 1 and 10.')
+
+while guessed_correctly == False:
+    get_number = int(input('What\'s the number? '))
+    if get_number == secret_number:
+        print('Yes! You win!')
+        guessed_correctly = True
+    elif get_number > secret_number:
+        print('%d is too high' % get_number)
+    else:
+        print('%d is too low' % get_number)
